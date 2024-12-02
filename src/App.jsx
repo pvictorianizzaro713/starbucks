28a8bcfa8792c71d43238905a2f589066579de12
pvictorianizzaro713 <paulanizzaro85@gmail.com>
2024-12-02 15:39:10 -0300

    atualizaçao

--- src/App.jsx
+++ src/App.jsx
@@ -5,3 +5,42 @@ export default function App() {
     </>
   )
 }
+import styled from "styled-components"
+
+export const MainStyle = styled.main`
+display: flex;
+align-items: center;
+height:80vh;
+`
+
+export const DivFuncional = styled.div`
+position: relative;
+
+`
+
+export const Copo = styled.img`
+position: relative;
+top: 400px;
+left: 100px;
+`
+
+export const Button = styled.button`
+  background-color: #037143;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 30px;
+  cursor: pointer;
+`;
+
+export const Span = styled.span`
+  color: #037143;
+  font-size: 56px;
+`;
+
+export const Text = styled.p`
+width: 60%;
+`
+export const Titulo = styled.h2`
+font-size: 40px;
+`
---
+++ src/style.jsx
@@ -0,0 +1,20 @@
+export const Button = styled.button`
+  background-color: #037143;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 30px;
+  cursor: pointer;
+`;
+
+export const Span = styled.span`
+  color: #037143;
+  font-size: 56px;
+`;
+
+export const Text = styled.p`
+width: 60%;
+`
+export const Titulo = styled.h2`
+font-size: 40px;
+`
